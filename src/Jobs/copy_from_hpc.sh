99eed0a4bb542838ef16d1d2bf16c8ba672dca26
mathias157 <mathiasros@proton.me>
2025-05-23 15:44:16 +0200

    Significant improvement of local download script

--- src/Jobs/copy_from_hpc.sh
+++ src/Jobs/copy_from_hpc.sh
@@ -4,3 +4,6 @@
 scp mberos@transfer.gbar.dtu.dk:/work3/mberos/BAF/src/Workflow/MetaResults/* Workflow/MetaResults
 scp mberos@transfer.gbar.dtu.dk:/work3/mberos/BAF/src/Workflow/OverallResults/* Workflow/OverallResults
 scp mberos@transfer.gbar.dtu.dk:/work3/mberos/BAF/src/Balmorel/base/model/MainResults_*.gdx Balmorel/base/model
+
+# Change local GAMS system directory in meta files
+ls Workflow/MetaResults/*_meta.ini | xargs sed -i 's|/appl/gams/47.6.0|/opt/gams/48.5|g'
