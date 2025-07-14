889b9225ceb472ba7ad40064180e36cb2faf43b2
Mathias157 <mathiasros@gmail.com>
2025-07-14 15:13:26 +0200

    Fixed script for exporting values to generative model

--- src/Workflow/Functions/export_data_to_generative_model.py
+++ src/Workflow/Functions/export_data_to_generative_model.py
@@ -25,6 +25,7 @@ import click
 
 def export_to_generative_model(scenario_folder: str, 
                                weather_year: int, 
+                               filename: str,
                                parameters: list = [
                                     'DH_VAR_T',
                                     'DE_VAR_T',
@@ -37,11 +38,11 @@ def export_to_generative_model(scenario_folder: str,
                                     'SOLEFLH', 
                                     'WTRRRFLH',
                                     'WTRRSFLH',
-                               ]
+                               ],
                                ):
     
     model = Balmorel('Balmorel')
-    model.load_incfiles(scenario_folder)
+    model.load_incfiles(scenario_folder, overwrite=True)
 
     # Verify weather year
     with open(f'Balmorel/{scenario_folder}/data/DH_VAR_T.inc', 'r') as f:
@@ -51,25 +52,48 @@ def export_to_generative_model(scenario_folder: str,
     if read_weather_year != weather_year:
         raise ValueError(f"Read weather year was {read_weather_year}, but process expect {weather_year}!")
 
-    input_data = {parameter : correct_format(symbol_to_df(model.input_data[scenario_folder], parameter), weather_year) for parameter in parameters}
-    
-    return input_data
+    print('Weather year', weather_year)
 
-def correct_format(df: pd.DataFrame, weather_year: int):
+    for parameter in parameters:
+        print(parameter)
+        df = correct_format(symbol_to_df(model.input_data[scenario_folder], parameter), parameter, weather_year)
+        
+        if parameter == parameters[0]:
+            df.T.to_csv(filename%weather_year, header=True)
+        else:
+            df.T.to_csv(filename%weather_year, mode='a', header=False)
+    
+
+def correct_format(df: pd.DataFrame, parameter: str, weather_year: int):
     
     # Extract unique dimensions
-    unique_sets = [col for col in df.columns if not(col in ['SSS', 'S', 'TTT', 'T'])]
+    unique_sets = [col for col in df.columns if not(col in ['SSS', 'TTT', 'Value'])]
+    # Remove unnecessary model years
+    if 'YYY' in df.columns:
+        df = df.query('YYY in ["2030", "2040", "2050"]')
     
     # Add sets
     df['WY'] = weather_year
-    df['Parameter'] = df[unique_sets].astype(str).agg("|".join, axis=1)
+    df['Parameter'] = parameter + "|" + df[unique_sets].astype(str).agg("|".join, axis=1)
+    
+    # Add missing temporal sets
+    if not('SSS' in df.columns):
+        S_values = [f'S{i:02d}' for i in range(1, 53)]
+        len_before = len(df)
+        df = pd.concat([df]*52, ignore_index=True)
+        df['SSS'] = np.tile(S_values, len_before)
+    if not('TTT' in df.columns):
+        T_values = [f'T{i:03d}' for i in range(1, 169)]
+        len_before = len(df)
+        df = pd.concat([df]*168, ignore_index=True)
+        df['TTT'] = np.tile(T_values, len_before)
     
     sum_before = df.Value.sum()
-    df = df.pivot_table(index=['WY', 'SSS', 'TTT'], columns='Parameter', values='Value', aggfunc='sum')
-    sum_after = df.sum()
+    df = df.pivot_table(index=['WY', 'SSS', 'TTT'], columns='Parameter', values='Value', aggfunc='sum', fill_value=0)
+    sum_after = df.sum().sum()
 
-    if sum_before != sum_after:
-        raise ValueError('Aggregation didnt work!')
+    if not(np.isclose(sum_before, sum_after, atol=0.1)):
+        raise ValueError(f'Aggregation didnt work!\nSum before: {sum_before}\nSum after: {sum_after}')
 
     return df
 
@@ -99,18 +123,17 @@ def main(scenario_folder: str):
                     2007, 2008, 2009, 2010, 2011, 
                     2012, 2013, 2014, 2015, 2016]
     
-    full_dataset = {}
-    for weather_year in [1982]:
+    for weather_year in weather_years:
         
         # Change weather year before preprocessing
         config.set('PreProcessing', 'balmorel_weather_year', str(weather_year))
         store_config(config)
         
         # Preprocess data
-        # os.system('pixi run preprocessing -F')
+        os.system('pixi run preprocessing -F --rerun-incomplete')
                 
         # Get parameters
-        full_dataset[weather_year] = export_to_generative_model(scenario_folder, weather_year)
+        export_to_generative_model(scenario_folder, weather_year, 'Pre-Processing/Output/genmodel_data_WY%d.csv')
 
 
 if __name__ == '__main__':
